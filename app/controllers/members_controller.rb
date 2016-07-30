1a74126d561ee64b995afd22be963ce1ae63e04b
emaren84 <emaren84@gmail.com>
2016-07-30 17:32:40 +0900

    Implement Memeber add function

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -1,11 +1,11 @@
 # Members Controller
 class MembersController < ApplicationController
-  before_action :find_meetup, only: [:add_members]
   def add_member
+    @meetup = find_meetup
     entry_members = member_params[:member_ids]
     if params_authorizable? && params_valid?
-      entry_members.each { |member| add_flow(member) }
-      render_200(add_user_reponse)
+      entry_members.each { |member| add_flow(member, @meetup) }
+      render_200(add_user_response(@meetup))
     elsif !params_valid?
       render_error_400
     else
@@ -17,43 +17,50 @@ class MembersController < ApplicationController
 
     def member_params
       params.require(:data).permit(:email, :messenger,
-                                   :messenger_room_id, :member_ids)
+                                   :messenger_room_id, member_ids: [])
     end
 
     def find_meetup
-      @meetup = MealMeetUp.find_by(messenger_room_id =>
+      @meetup = MealMeetUp.find_by(messenger_room_id:
                                    member_params[:messenger_room_id])
     end
 
-    def add_flow(member)
+    def add_flow(member, meetup)
       user = User.create(service_uid: member)
-      user_log = UserLog.create(user_id: user.id)
+      user_log = MealLog.create(user_id: user.id)
       UserMessenger.create(user_id: user.id,
                            messenger_code: load_messenger_code)
       MealMeetUpTask.create(meal_log_id: user_log.id,
-                            meal_meet_up_id: @meetup.id,
+                            meal_meet_up_id: meetup.id,
                             task_status: init_task_code)
     end
 
-    def add_user_reponse
+    def add_user_response(meetup)
       { data:
-        { email: @meetup.admin_id.service_uid,
-          messenger: @meetup.admin.user_messenger.messenger,
-          messenger_room_id: @meetup.messenger_room_id,
-          member_ids: member_params[:member_ids] } }
+        { email: meetup.admin.service_uid,
+          messenger: meetup.messenger.value,
+          messenger_room_id: meetup.messenger_room_id,
+          member_ids: get_members_list(meetup) } }
+    end
+
+    # MeetUp에 속한 Task를 가지고 있는 멤버들의 명단을 가져온다.
+    def get_members_list(meetup)
+      tasks = meetup.meal_meet_up_tasks
+      tasks.map { |task| task.meal_log.user.service_uid }
     end
 
     def load_messenger_code
       CodeTable.find_messenger(member_params[:messenger]).id
     end
 
+    # Task가 만들어질땐 'unpaid'상태로 등록.
     def init_task_code
       CodeTable.find_task('unpaid').id
     end
 
     def params_valid?
-      !meetup_params[:messenger_room_id].to_s.empty? &&
-        !meetup_params[:member_ids].empty?
+      !member_params[:messenger_room_id].to_s.empty? &&
+        !member_params[:member_ids].empty?
     end
 
     def params_authorizable?
