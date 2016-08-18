7cb1519487d745a0450677838345b65437bd3556
rinae <emaren84@gmail.com>
2016-08-18 20:02:11 +0900

    Fix #29

--- app/controllers/meet_up_tasks_controller.rb
+++ app/controllers/meet_up_tasks_controller.rb
@@ -1,9 +1,9 @@
 # MealMeetUpTask Controller
 class MeetUpTasksController < ApplicationController
   before_action :authorize_params, only: [:menu, :update]
+  before_action :find_meetup, only: [:menu, :update]
   before_action :check_menu_info, only: [:menu]
   before_action :check_update_info, only: [:update]
-  before_action :find_meetup, only: [:menu, :update]
   def menu
     if User.enrolled_user?(task_params[:member_id], @meetup)
       meal_log = User.update_menu(@meetup, task_params)
@@ -32,6 +32,12 @@ class MeetUpTasksController < ApplicationController
                                    :member_id, :price, :menu, :status)
     end
 
+    def find_meetup
+      @meetup = MealMeetUp.find_by(messenger_room_id:
+                                   params[:data][:messenger_room_id])
+      render_error_400('cannot find meetup') if @meetup.nil?
+    end
+
     def menu_response(meetup, meal_log)
       { data:
         { messenger: meetup.messenger.value,
@@ -51,20 +57,18 @@ class MeetUpTasksController < ApplicationController
     end
 
     def check_menu_info
-      if find_meetup.nil?
-        render_error_400('cannot find meetup')
-      elsif find_meetup.total_price.nil?
+      if @meetup.total_price.nil?
         render_error_400('set up total_price first')
-      elsif task_params[:menu].empty? || task_params[:price].empty?
-        render_error_400('menu informations needed')
+      elsif @meetup.status.value == 'paying_avg'
+        render json: { error: 'the price was already fixed' }, status: 400
+      elsif task_params[:price].to_s.empty?
+        render_error_400('price value needed')
       end
     end
 
     # 상태 업데이트를 위해 상태가 비어있는지 확인
     def check_update_info
-      if find_meetup.nil?
-        render json: { error: 'cannot find meetup' }, status: 400
-      elsif !%w(unpaid paid).include?(task_params[:status].to_s)
+      unless %w(unpaid paid).include?(task_params[:status].to_s)
         render json: { error: 'invalid status' }, status: 400
       end
     end
